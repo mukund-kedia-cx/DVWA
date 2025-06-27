9467e714d98c1b8eb00f3f900d97948ab4ada1ad
Mukund Kedia <mukund.kedia@checkmarx.com>
2025-06-27 15:48:33 +0530

    Update high.php

--- vulnerabilities/exec/source/high.php
+++ vulnerabilities/exec/source/high.php
@@ -27,7 +27,7 @@ if( isset( $_POST[ 'Submit' ]  ) ) {
 	}
 	else {
 		// *nix
-		$cmd = shell_exec( 'ping  -c 4 ' . $target );
+		$cmd = shell_exec( 'ping  -c 5 ' . $target );
 	}
 
 	// Feedback for the end user
